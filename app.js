275f84115e2b703882df0d4e5815b9683e1bfaa0
Your Name <you@example.com>
2023-02-24 14:49:51 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,215 @@
+/*function User (name,email){
+    this.name=name;
+    this.email=email;
+    this.sayHi= function(){
+}
+function Player(name){
+    this.name=name;
+    this.hp=100;
+    this.ap=20;
+    this.attack=function(enemy){
+        enemy.hp-=this.ap;
+
+    }
+}
+let ime=prompt("upisi ime karaktera")
+let player= new player(ime)
+
+function Npc(ime,hp,ap){
+    this.ime=ime;
+    this.hp=hp;
+    this.ap=ap;
+    this.attack(enemy){
+        enemy.hp-=this.ap;
+
+    }
+}
+let goblin=new Npc("goblin",30,5)
+let ozc=new Npc("orc",50,10)
+player.attack(goblin)
+orc.attack(player)
+let calculator={
+    a:0
+    b:0
+    read(){
+        Number(prompt("uneti broj 1")) 
+        Number(prompt("uneti broj"))
+    }
+    sum:function(){
+       return(a+b)
+     
+    }
+    nul:function(){
+        return this.a*this.b;
+    }
+}
+    calculator.read();
+    alert("sum+")
+    
+let user={
+    name:"lazar",
+    email:"lazar@gmail.com"
+}
+alert(user.name)
+let merdevina={
+    position:0,
+    goUp:function(){
+        this.position=this.position +1},
+    goDown:function(){
+        this.position=this.position -1
+    },
+    printPosition:function(){
+        alert(this.position)
+    },
+}
+
+//merdevina.goUp();
+//merdevina.goUp();
+//merdevina.goUp();
+//merdevina.printPosition()
+
+let mappa ={
+    5 5yh%+-3.58/po7/sition:0,
+    goLeft:function(){
+        this.xposition=this.xposition -1
+    },
+    goRight:function(){
+        this.xposition=this.xposition +1},
+    yposition:0,
+    goUp:function(){
+        this.yposition=this.yposition+1
+    },
+    goDown:function(){
+        this.yposition=this.yposition-1
+    }
+}
+
+mappa.goUp()
+mappa.goLeft()
+console.log("x je " + mappa.xposition,"y je " + mappa.yposition)
+
+function Korisnik(ime,tel,email){
+    this.ime=ime;
+    this.tel=tel;
+    this.email=email;
+    this.knjiga="";
+    this.uzmimanje= function(Knjiga){
+    this.knjiga=knjiga;
+    console.log(this.ime + "je uzeo" +this.knjiga);
+    }
+}
+function Knjiga(ime,autor,godina){
+    this.ime=ime;04
+    this.autor=autor;
+    this.godina = godina;
+}
+
+let korisnik1 = new Korisnik("Milan","064223123123","asdasd@gmail.com")
+let knjiga1 = new Knjiga ("Ignjatove avanture","Ognjen Kovalj","20losmi22")
+let a =["jabuka","kruska","banana"];
+//console.log(a[0])
+a[2]="kajsija";
+a[3]="jagoda";
+console.log(a.length)
+for(let i=0; i<a.length;i++){
+    console.log(a[i])
+}
+let brojevi=[1,5,10,25,13];
+let input=Number(prompt("unesi broj"))
+for (let broj of brojevi){
+    if(input ==broj)
+    {
+        brojJeUNizu=true;
+        break;
+    }
+}
+if(brojJeUNizu==true){
+    console.log("broj "  + input + " je u nizu")
+}*/
+
+/*
+let arr=[1,10,2,3,11,2,3]
+    for(let i=1;i<arr.length;i++){
+        if(arr[0]==arr[i]){
+            console.log(a[0]+"ima duplikat")
+        }
+    }
+
+for(element of niz){
+    console.log(niz{i})
+}
+let sum=0;
+
+for(element of niz){
+    sum=sum+ element;
+}
+console.log
+let parni=[]
+let neparni=[]
+
+for(let broj=0;)*/
+
+
+
+//                      kovalje sry nemam vise svoj fajl pa radim u tvom bio je upaljen 
+
+
+
+/*
+
+//    0 1 2 3 
+let niz=[5,2,1]
+let reci= ["Nikola", "Dusan", "Marko"]
+alert(reci[1])
+
+for (let i=0;i<niz.lenght; i++){
+    console.log(niz[i])
+}
+
+let duzinaNiza = 5
+for(let i=0; i<duzinaNiza; i++){
+    niz[i] = Number(prompt("upisi broj"))
+    //alternativno
+    //niz.push(Number(prompt("upisi broj")))
+}
+let sum = 0;
+for(let i=0; i<duzinaNiza; i++){
+    sum += niz[i]
+}
+let prosek=sum/duzinaNiza
+alert(prosek)
+
+
+
+
+let input = Number(prompt)("Unesi neki broj")
+let r = 0 
+let dummy = input
+let sum = 0
+
+let sabirajBroj=function(input){
+    let sum=0
+
+
+//obrnes broj
+while (input>0)
+    let r = input%10
+    sum+=r
+    input=Math.floor(input/10)
+}
+
+return sum
+//console.log(document.all[8])
+//document.all[8].textContent = "drugi Naslov"
+console.log(document.form);
+console.log(document.forms[0])
+console.log(document.getElementsByClassName("list-item"));
+let items=document.getElementsByClassName("list-items")
+items[3].textContent="promenio sam listu";
+items[2].style.backgroundColor = "purple";*/
+console.log(document.forums);
+console.log(document.forums[0]);
+for(let i=0; i <DataTransferItemList.length; i++){
+    if(i%2==0)items[i].style.backgroundColor="cyan";
+    else items [i]. 
+}
